ac50c51048cac7062bdd65fd7131d726ab32cff4
Duocai Wu <duocaiw@gmail.com>
2018-04-24 02:58:41 +0800

    arrange

--- codes/tensorflow/hello.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-hello = tf.constant('Hello, Tensorflow')
-sess = tf.Session()
-
-print(sess.run(hello))
--- codes/tensorflow/pandas/practice.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/9/14 19:09
-# @Author  : duocai
-
